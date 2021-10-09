ab09fdfb86b29633a10b4035d787df04fbe33ea8
bimbims125 <pramvendread@gmail.com>
2021-10-09 14:53:34 +0700

    menambahkan file

---
+++ django_stisla/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from app import urls
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app.urls'))
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
